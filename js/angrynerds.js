cd86ebd65fb5638b63fcbbb51a622d474d37fb32
Vitaliano Palmieri Neto <vitaliano.neto@gmail.com>
2015-08-13 12:26:13 -0300

    Cannon and cannon base on the right height

--- js/angrynerds.js
+++ js/angrynerds.js
@@ -3,38 +3,72 @@ var canvasWidth = gamecanvas.attr('width');
 var canvasHeight = gamecanvas.attr('height');
 var context2d = gamecanvas.get(0).getContext("2d");
 
-var imageScale = 0;
+var imageScale = canvasWidth / /*bg width*/ 2211;
+var resourcesLoaded = 0;
 var backgroundImage = new Image();
 backgroundImage.loaded = false;
 backgroundImage.onload = function () {
-	backgroundImage.loaded = true;
+  resourcesLoaded += 1;
 };
 backgroundImage.src = "img/cenario.png";
-imageScale = canvasWidth / /*bg width*/ 2211;
+
 
 var schoolImage = new Image();
-schoolImage.loaded = false;
 schoolImage.onload = function () {
-	schoolImage.loaded = true;
+  resourcesLoaded += 1;
   schoolImage.width *= imageScale;
   schoolImage.height *= imageScale;
 };
 schoolImage.src = "img/escola.png";
 
-var lastMillis = new Date().getTime()
+var cannonBaseImage = new Image();
+cannonBaseImage.onload = function () {
+  resourcesLoaded += 1;
+  cannonBaseImage.width *= imageScale;
+  cannonBaseImage.height *= imageScale;
+};
+cannonBaseImage.src = "img/suporte_canhao.png";
 
+var cannon = {
+    imgPath: "img/canhao.png",
+    img: new Image(),
+    angle: 30,
+    load: function(){
+      this.img.onload = function() {
+        resourcesLoaded += 1;
+        this.width *= imageScale;
+        this.height *= imageScale;
+      };
+      this.img.src = this.imgPath;
+    },
+    draw: function () {
+      context2d.drawImage(this.img, 160, 570,
+                          this.img.width, this.img.height);
+    }
+};
+cannon.load();
+
+var alfredo = {
+  imgPath: "img/alfredo.png",
+  x: 0,
+  y: 0,
+};
+
+var lastMillis = new Date().getTime()
 function update() {
   var currentMillis = new Date().getTime();
   var deltaMillis = currentMillis - lastMillis;
   lastMillis = currentMillis;
 
-  if (backgroundImage.loaded) {
+  if(resourcesLoaded >= 4) {
 		context2d.drawImage(backgroundImage, 0, 0, canvasWidth, canvasHeight);
-	}
-  if (schoolImage.loaded) {
     context2d.drawImage(schoolImage, canvasWidth-schoolImage.width, canvasHeight-schoolImage.height,
                         schoolImage.width, schoolImage.height);
+    cannon.draw();
+    context2d.drawImage(cannonBaseImage, 160, 620,
+                        cannonBaseImage.width, cannonBaseImage.height);
   }
+
 }
 
 setTimeout(update, 33);
