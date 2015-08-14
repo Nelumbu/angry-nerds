cce96cf71f74a575a3551463f6af3d571157130e
Vitaliano Palmieri Neto <vitaliano.neto@gmail.com>
2015-08-14 19:40:32 -0300

    Cannon folowing the mouse

--- js/angrynerds.js
+++ js/angrynerds.js
@@ -3,6 +3,9 @@ var canvasWidth = gamecanvas.attr('width');
 var canvasHeight = gamecanvas.attr('height');
 var context2d = gamecanvas.get(0).getContext("2d");
 
+function toRadians(degrees){return degrees * Math.PI / 180;}
+function toDegrees(radians){return radians / Math.PI * 180;}
+
 var imageScale = canvasWidth / /*bg width*/ 2211;
 var resourcesLoaded = 0;
 var backgroundImage = new Image();
@@ -32,27 +35,48 @@ cannonBaseImage.src = "img/suporte_canhao.png";
 var cannon = {
     imgPath: "img/canhao.png",
     img: new Image(),
-    angle: -25,
+    angle: toRadians(-25),
+    pivot: {
+      x: 0,
+      y: 0
+    },
+    x: 150,
+    y: 600,
     load: function(){
+      var parent = this;
       this.img.onload = function() {
         resourcesLoaded += 1;
         this.width *= imageScale;
         this.height *= imageScale;
+        parent.pivot.x = this.width*0.333;
+        parent.pivot.y = this.height*0.5;
       };
       this.img.src = this.imgPath;
     },
     draw: function () {
       context2d.save();
       //context2d.setTransform(1, 0, 0, 1, 0, 0);
-      context2d.translate(150+this.img.width*0.333, 600+this.img.height*0.5);
-      var rad = this.angle * Math.PI / 180;
+      context2d.translate(this.x+this.pivot.x, this.y+this.pivot.y);
+      var rad = this.angle;
+      //console.log("angle:", rad / Math.PI * 180)
       context2d.rotate(rad);
-      context2d.drawImage(this.img, -this.img.width*0.333, -this.img.height*0.5,
+      context2d.drawImage(this.img, -this.pivot.x, -this.pivot.y,
                           this.img.width, this.img.height);
       context2d.restore();
     }
 };
 cannon.load();
+
+var mouse = {
+  x: 0,
+  y: 0
+};
+
+document.onmousemove = function(e){
+    mouse.x = e.pageX;
+    mouse.y = e.pageY;
+    cannon.angle = Math.max(toRadians(-90) , Math.min(Math.atan2(mouse.y-(cannon.y+cannon.pivot.y), mouse.x-(cannon.x+cannon.pivot.x)), 0));
+}
 
 var alfredo = {
   imgPath: "img/alfredo.png",
@@ -77,4 +101,4 @@ function update() {
 
 }
 
-setTimeout(update, 33);
+var loopInterval = setInterval(update, 33);
