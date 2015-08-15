703bfd749c955521ad3f8f24fa65fdcfd9def602
Vitaliano Palmieri Neto <vitaliano.neto@gmail.com>
2015-08-14 21:20:50 -0300

    Force arrow working on click and hold and stopping at release

--- js/angrynerds.js
+++ js/angrynerds.js
@@ -15,7 +15,6 @@ backgroundImage.onload = function () {
 };
 backgroundImage.src = "img/cenario.png";
 
-
 var schoolImage = new Image();
 schoolImage.onload = function () {
   resourcesLoaded += 1;
@@ -23,6 +22,14 @@ schoolImage.onload = function () {
   schoolImage.height *= imageScale;
 };
 schoolImage.src = "img/escola.png";
+
+var forceImage = new Image();
+forceImage.onload = function () {
+  resourcesLoaded += 1;
+  forceImage.width *= imageScale;
+  forceImage.height *= imageScale;
+};
+forceImage.src = "img/barra_de_forca.png";
 
 var cannonBaseImage = new Image();
 cannonBaseImage.onload = function () {
@@ -67,6 +74,51 @@ var cannon = {
 };
 cannon.load();
 
+var forceArrow = {
+  imgPath: "img/seta_forca.png",
+    img: new Image(),
+    x: 50,
+    y: canvasHeight * 0.5,
+    maxY: 0,
+    minY: 0,
+    force: 0,
+    signal: 1,
+    active: false,
+    load: function(){
+      this.img.onload = function() {
+        resourcesLoaded += 1;
+        this.width *= imageScale;
+        this.height *= imageScale;
+      };
+      this.img.src = this.imgPath;
+    },
+    draw: function () {
+      if (this.maxY == 0 || this.minY == 0) {
+        this.minY = 10+canvasHeight*0.5-forceImage.height*0.5;
+        this.maxY = this.minY + forceImage.height-10;
+      }
+      this.y = this.maxY - (this.maxY - this.minY) * this.force;
+      context2d.drawImage(this.img, this.x, this.y-this.img.height*0.5,
+                          this.img.width, this.img.height);
+    },
+    update: function (deltaMillis) {
+      if(this.active){
+        this.force += deltaMillis/1000 * this.signal;
+        if (this.force >= 1 && this.signal > 0){
+          this.force = 1;
+          this.signal *= -1;
+        } else if (this.force <= 0 && this.signal < 0) {
+          this.force = 0;
+          this.signal *= -1;
+        }
+      } else {
+        this.force = 0;
+        this.signal = 1;
+      }
+    }
+};
+forceArrow.load();
+
 var mouse = {
   x: 0,
   y: 0
@@ -76,6 +128,18 @@ document.onmousemove = function(e){
     mouse.x = e.pageX;
     mouse.y = e.pageY;
     cannon.angle = Math.max(toRadians(-90) , Math.min(Math.atan2(mouse.y-(cannon.y+cannon.pivot.y), mouse.x-(cannon.x+cannon.pivot.x)), 0));
+}
+
+document.onmousedown = function(e){
+    if(e.button == 0){
+      forceArrow.active = true;
+    }
+}
+
+document.onmouseup = function(e){
+    if(e.button == 0){
+      forceArrow.active = false;
+    }
 }
 
 var alfredo = {
@@ -94,9 +158,13 @@ function update() {
     context2d.drawImage(backgroundImage, 0, 0, canvasWidth, canvasHeight);
     context2d.drawImage(schoolImage, canvasWidth-schoolImage.width, canvasHeight-schoolImage.height,
                         schoolImage.width, schoolImage.height);
+    context2d.drawImage(forceImage, 20, canvasHeight*0.5-forceImage.height*0.5,
+                        forceImage.width, forceImage.height);
     cannon.draw();
     context2d.drawImage(cannonBaseImage, 160, 620,
                         cannonBaseImage.width, cannonBaseImage.height);
+    forceArrow.update(deltaMillis);
+    forceArrow.draw();
   }
 
 }
